9a9fb92da1759dafba108ef1ee7563ae5142b5e9
luckydaily <luckydailyss@gmail.com>
2017-05-22 23:10:53 +0900

    add : enemy 스크립트 추가

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float fTimeBetweenAttack = 0.5f;
+    public int iAttackDamage = 10;
+
+    GameObject player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+
+    bool bPlayerInRange;
+    float fTimer;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject == player)
+        {
+            bPlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject == player)
+        {
+            bPlayerInRange = false;
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        fTimer = Time.deltaTime;
+        if(fTimer >= fTimeBetweenAttack && bPlayerInRange == true && enemyHealth.iCurrentHealth > 0)
+        {
+            Attack();
+        }
+	}
+
+    void Attack()
+    {
+        fTimer = 0f;
+
+        if(playerHealth.iCurrentHealth > 0)
+        {
+            playerHealth.TakeDamage(iAttackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int iHealth = 100;
+    public int iCurrentHealth;
+
+    public float fFlashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+    public float fSinkSpeed = 1f;
+
+    bool bIsDead = false;
+    bool bIsSinking = false;
+    bool bDamaged = false;
+
+    void Awake()
+    {
+        iCurrentHealth = iHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        bDamaged = true;
+        iCurrentHealth -= amount;
+
+        if(iCurrentHealth <= 0 && bIsDead == false)
+        {
+            Death();
+        }
+    }
+
+    public IEnumerator StartDamage(int damage, Vector3 playerPosition, float delay, float pushBack)
+    {
+        yield return new WaitForSeconds(delay);
+
+        try
+        {
+            TakeDamage(damage);
+
+            Vector3 diff = playerPosition - transform.position;
+            diff = diff / diff.sqrMagnitude;
+            GetComponent<Rigidbody>().AddForce((transform.position - new Vector3(diff.x, diff.y, 0f)) * 50f * pushBack);
+        }catch(MissingReferenceException e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    void Update()
+    {
+        if(bDamaged == true)
+        {
+            transform.GetChild(0).GetComponent<Renderer>().material.SetColor("OutlineColor", flashColor);
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_OutlineColor", 
+                Color.Lerp(transform.GetChild(0).GetComponent<Renderer>().material.GetColor("_OutlineColor"), Color.black, fFlashSpeed * Time.deltaTime));
+        }
+
+        bDamaged = false;
+
+        if(bIsSinking == true)
+        {
+            transform.Translate(-Vector3.up * fSinkSpeed * Time.deltaTime);
+        }
+    }
+
+    void Death()
+    {
+        bIsDead = true;
+
+        transform.GetChild(0).GetComponent<BoxCollider>().isTrigger = true;
+        StartSinking();
+    }
+
+    public void StartSinking()
+    {
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        bIsSinking = true;
+    }
+}
